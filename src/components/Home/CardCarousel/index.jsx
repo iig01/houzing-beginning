8ff31898170b3c2ab301bf6c6f0c31c71d6900a1
igaybullaev <77196863+igaybullaev@users.noreply.github.com>
2022-07-20 11:04:39 +0500

    test

--- src/components/Home/CardCarousel/index.jsx
+++ src/components/Home/CardCarousel/index.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export const CardCarousel = () => {
   return (
     <div>
-      <div className="title">Recomended</div>
+      <div className="title">Ismail aka</div>
       <div className="description">
         Nulla quis curabitur velit volutpat auctor bibendum consectetur sit.
       </div>
